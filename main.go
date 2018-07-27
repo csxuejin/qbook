6d832c0aea0f3982fc9479b9c19fd865aa27ddfd
csxuejin <602296435@qq.com>
2018-07-27 17:18:39 +0800

    global kodoClient

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ type Config struct {
 	Kodo    *kodo.Config `json:"kodo"`
 }
 
+var (
+	kodoClient *kodo.Kodo
+)
+
 func main() {
 	config, err := initConfig()
 	if err != nil {
@@ -28,28 +32,29 @@ func main() {
 		return
 	}
 
-	kodoClient := kodo.New(config.Kodo)
+	kodoClient = kodo.New(config.Kodo)
 	for _, v := range files {
 		if v.IsDir() {
-			helper(dirname+"/"+v.Name(), v.Name(), v, kodoClient)
+			helper(dirname+"/"+v.Name(), v.Name(), v)
 		} else {
 			err = kodoClient.PutFile(v.Name(), dirname+"/"+v.Name())
 			if err != nil {
-				log.Printf("loload failed: %v\n", dirname+"/"+v.Name())
+				log.Printf("upload failed: %v\n", dirname+"/"+v.Name())
 			}
 		}
 	}
 }
 
-func helper(filePath, prefix string, f os.FileInfo, kodoClient *kodo.Kodo) {
+func helper(filePath, prefix string, f os.FileInfo) {
 	files, err := ioutil.ReadDir(filePath)
 	if err != nil {
 		log.Fatalf("ioutil.ReadDir(%v): %v", filePath, err)
 		return
 	}
+
 	for _, v := range files {
 		if v.IsDir() {
-			helper(filePath+"/"+v.Name(), prefix+"/"+v.Name(), v, kodoClient)
+			helper(filePath+"/"+v.Name(), prefix+"/"+v.Name(), v)
 		} else {
 			var key string
 			if prefix == "" {
@@ -58,8 +63,12 @@ func helper(filePath, prefix string, f os.FileInfo, kodoClient *kodo.Kodo) {
 				key = prefix + "/" + v.Name()
 			}
 
-			log.Printf("upload successfully: %v\n", key)
 			err = kodoClient.PutFile(key, filePath+"/"+v.Name())
+			if err != nil {
+				log.Fatalf("upload failed: %v\n", key)
+			} else {
+				log.Printf("upload successfully: %v\n", key)
+			}
 		}
 	}
 }
